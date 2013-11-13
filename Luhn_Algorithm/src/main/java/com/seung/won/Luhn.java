f3579a4a03101d2431f88b827365425650035d43
Seungwon Kim <closer527@gmail.com>
2013-11-14 02:57:02 +0900

    create Luhn class

---
+++ Luhn_Algorithm/src/main/java/com/seung/won/Luhn.java
@@ -0,0 +1,11 @@
+package com.seung.won;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: seung-wongim
+ * Date: 2013. 11. 14.
+ * Time: 오전 12:51
+ * To change this template use File | Settings | File Templates.
+ */
+public class Luhn {
+}
---
+++ Luhn_Algorithm/src/test/java/com/seung/won/LuhnTest.java
@@ -0,0 +1,17 @@
+package com.seung.won;
+
+import org.junit.Test;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: seung-wongim
+ * Date: 2013. 11. 14.
+ * Time: 오전 12:51
+ * To change this template use File | Settings | File Templates.
+ */
+public class LuhnTest {
+    @Test
+    public void testLuhnAlgorithm() {
+
+    }
+}
