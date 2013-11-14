2b7213e893b2795d952877e7b2711e71230741f7
Seungwon Kim <closer527@gmail.com>
2013-11-14 16:27:08 +0900

    complete Luhn algorithm

--- Luhn_Algorithm/src/main/java/com/seung/won/Luhn.java
+++ Luhn_Algorithm/src/main/java/com/seung/won/Luhn.java
@@ -8,4 +8,55 @@ package com.seung.won;
  * To change this template use File | Settings | File Templates.
  */
 public class Luhn {
+    private String cardStr;
+    private int[] cardChars;
+    private int checksum;
+
+    public Luhn() {
+        checksum = 0;
+    }
+    public Luhn(String card){
+        cardStr = card;
+        checksum = 0;
+        cardChars = new int[16];
+    }
+
+
+    public int getLength() {
+        return cardStr.length();
+    }
+
+    public void intoArray() {
+        for (int i=0; i<cardStr.length(); i++)
+            cardChars[i] = Integer.parseInt(String.valueOf(cardStr.charAt(i)));
+    }
+
+    public boolean isValid() {
+        if(getLength() != 16) return false;
+
+        for (int i=14; i>0; i--) {
+            if ((i+1)%2 == 0) {  // 짝수일 때
+                int temp = cardChars[i] * 2;
+                if (temp >= 10)
+                    checksum += (temp/10) + (temp%10);
+                else
+                    checksum += temp;
+            }
+            else {
+                checksum += cardChars[i];
+            }
+        }
+        System.out.printf("checksum is : %d\n", checksum);
+
+        int ceilingSum = (checksum/10 + 1) * 10;
+
+        System.out.printf("Celing Sum is : %d\n", ceilingSum);
+
+        if ((ceilingSum-checksum)%10 != cardChars[15]) {
+            System.out.println("is not correct\n");
+            return false;
+        }
+
+        return true;  //To change body of created methods use File | Settings | File Templates.
+    }
 }
--- Luhn_Algorithm/src/test/java/com/seung/won/LuhnTest.java
+++ Luhn_Algorithm/src/test/java/com/seung/won/LuhnTest.java
@@ -1,5 +1,6 @@
 package com.seung.won;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 /**
@@ -12,6 +13,22 @@ import org.junit.Test;
 public class LuhnTest {
     @Test
     public void testLuhnAlgorithm() {
+        Luhn luhn = new Luhn("1234567890123456");
 
+        Assert.assertEquals(16, luhn.getLength());
+    }
+
+    @Test
+    public void isValid() {
+        Luhn luhn = new Luhn("1234567890123456");
+
+        Assert.assertEquals(true, luhn.isValid());
+    }
+
+    @Test
+    public void test() {
+        Luhn luhn = new Luhn("1234567890123456");
+
+        luhn.intoArray();
     }
 }
